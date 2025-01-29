edc53803f2a306855bfe8cc67fa96e284597aa41
lambertzz <zjzhangg@gmail.com>
2025-01-29 15:41:22 -0800

    half if IE3

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,7 +1,7 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
 import Table from "./Table";
 import Form from "./Form";
+import React, { useState, useEffect } from "react";
 
 function MyApp() {
   const [characters, setCharacters] = useState([
@@ -25,12 +25,39 @@ function MyApp() {
   ]);
 
 
-  const updateList = (person) => {
-    setCharacters([...characters, person]); // Add the new person to the list
-  };
+  function updateList(person) {
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
 
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(person)
+    });
+  
+    return promise;
+  }
 
 function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
